88ab719621e318a63cbcee356f6cbc605deb07a1
Paulo Ricardo Pinto Godoy <paulo.godoy@pampa.compasso>
2019-09-26 16:57:43 -0300

    atualizado

--- Java-io/src/br/com/alura/io/teste/TesteCopiarArquivo.java
+++ Java-io/src/br/com/alura/io/teste/TesteCopiarArquivo.java
@@ -2,6 +2,7 @@ package br.com.alura.io.teste;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,29 +15,27 @@ import java.io.Writer;
 public class TesteCopiarArquivo {
 
 	public static void main(String[] args) throws IOException {
-		//Fluxo de entrada com arquivo
-		
-		InputStream fis = System.in;
-		
-		Reader isr = new InputStreamReader(fis);
-		BufferedReader br = new BufferedReader(isr);
-		
-		
-		OutputStream fos = new FileOutputStream("arg4.txt");
-		Writer osr = new OutputStreamWriter(fos);
-		BufferedWriter bw = new BufferedWriter(osr);
-		
-		String linha = br.readLine();
-		
-		while(linha != null && linha.isEmpty()) {
-		bw.write(linha);
-		bw.newLine();
-		linha = br.readLine();
-		}
-	
-		br.close();
-		bw.close();
+		  InputStream fis = System.in; //new FileInputStream("lorem.txt");
+          Reader isr = new InputStreamReader(fis);
+          BufferedReader br = new BufferedReader(isr);
 
-	}
+          OutputStream fos = new FileOutputStream("lorem2.txt"); //System.out;
+          Writer osw = new OutputStreamWriter(fos);
+          BufferedWriter bw = new BufferedWriter(osw);
+
+          String linha = br.readLine();
+
+          while(linha != null && !linha.isEmpty()) {
+
+              bw.write(linha);
+              bw.newLine();
+              bw.flush();
+              linha = br.readLine();
+          }
+
+          br.close();
+          bw.close();
+
+  }
 
 }
