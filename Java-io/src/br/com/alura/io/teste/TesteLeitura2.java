c913ebe6312ba27e3a18727edc54ea29a37101ff
Paulo Ricardo Pinto Godoy <paulo.godoy@pampa.compasso>
2019-09-27 10:15:01 -0300

    Parseando arquivo parte 2

--- Java-io/src/br/com/alura/io/teste/TesteLeitura2.java
+++ Java-io/src/br/com/alura/io/teste/TesteLeitura2.java
@@ -1,7 +1,7 @@
 package br.com.alura.io.teste;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -10,15 +10,38 @@ public class TesteLeitura2 {
 		
 		Scanner ler = new Scanner(new File("contas.csv"));
 		
+		//Scanner para ler linha
+		
+		
 		
 		while(ler.hasNextLine()) {
 			String arquivo = ler.nextLine();
 			System.out.println(arquivo);
 			
-			String[] valores = arquivo.split(",");
-			System.out.println(Arrays.toString(valores));
+//			String[] valores = arquivo.split(",");
+//			System.out.println(Arrays.toString(valores));
+//			
+//			System.out.println(valores[3]);
 			
-			System.out.println(valores[3]);
+			Scanner linhaScanner = new Scanner(arquivo);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String valor1 = linhaScanner.next();
+			int valor2 = linhaScanner.nextInt();
+			int valor3 = linhaScanner.nextInt();
+			String valor4 = linhaScanner.next();
+			double valor5 = linhaScanner.nextDouble();
+			
+			
+		
+			
+			System.out.println("Conta: " + valor1 + " Agencia: " + valor2 + " Numero da conta: " + valor3 + " Titular: " + valor4 + " Saldo: " + valor5 );
+			
+			
+			
+			linhaScanner.close();
+			
 		}
 		
 		
