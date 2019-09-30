7f3466df763efffd7e365b4bf5d0ee747386bdc3
Paulo Ricardo Pinto Godoy <paulo.godoy@pampa.compasso>
2019-09-30 18:20:59 -0300

    colections2

--- Gerenciador-de-cursos/src/br/com/alura/Aluno.java
+++ Gerenciador-de-cursos/src/br/com/alura/Aluno.java
@@ -1,30 +1,26 @@
 package br.com.alura;
 
 public class Aluno {
-	private String nome;
-	private int matricula;
-	
-	
-	public Aluno(String nome, int matricula) {
-		this.nome = nome;
-		this.matricula = matricula;
-	}
 
+	private String nome;
+	private int numeroMatricula;
+
+	public Aluno(String nome, int numeroMatricula) {
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
 
 	public String getNome() {
 		return nome;
 	}
 
-
-	
-
-	public int getMatricula() {
-		return matricula;
+	public int getNumeroMatricula() {
+		return numeroMatricula;
 	}
 
-
-	
-	
-	
-
+	@Override
+	public String toString() {
+		return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula
+				+ "]";
+	}
 }
--- Gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ Gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,14 +1,17 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -27,6 +30,10 @@ public class Curso {
 		return Collections.unmodifiableList(aulas);
 	}
 
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
@@ -39,6 +46,10 @@ public class Curso {
 	public String toString() {
 		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal()
 				+ ", aulas: + " + this.aulas + "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
 	}
 
 }
