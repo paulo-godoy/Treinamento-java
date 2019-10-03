3efba2a4a3ef7886907744e2d4bf5b616c6acb11
Paulo Ricardo Pinto Godoy <paulo.godoy@pampa.compasso>
2019-10-03 18:11:43 -0300

    servlet1

---
+++ gerenciador/src/br/com/aluragerenciador/servlet/OlaMundo.java
@@ -0,0 +1,17 @@
+package br.com.aluragerenciador.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet
+public class OlaMundo extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+
+}
